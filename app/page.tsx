88e401fc7967d6d1e4acb3ad5e08c864b2b307cc
Poshitha Ravindu <raviposhitha@gmail.com>
2024-08-09 20:58:42 +0530

    fix: name change

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello mother fucker</h1>
+      <h1>Hello Have a nice Joke</h1>
     </main>
   );
 }
